f7236bea49c077fb24316e41a3b5b0e5d3825be4
Abner da Silva Santos <93294372+AbnerSantos25@users.noreply.github.com>
2024-09-12 13:17:52 -0300

    Update calcular.php

--- calcular.php
+++ calcular.php
@@ -1,6 +1,15 @@
 <?php
-function calcularFrete($peso, $largura, $altura, $comprimento, $valor) {
+    header('Access-Control-Allow-Origin: *');
+    header('Content-Type: application/json');
+
     $token = "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiIxIiwianRpIjoiNzlkMjM1NWMwMWQxMGQ4NWZkZjgzYzMzYjJlOWRlMTViMzFhOWI3YmFhYzg1ODUzMmI4NGQ3ZjM3MjdhNmQwZTliNjg0Y2U1OTYxZmQ4ZWUiLCJpYXQiOjE3MjU5MDM5MDQuOTIyNDI1LCJuYmYiOjE3MjU5MDM5MDQuOTIyNDI3LCJleHAiOjE3NTc0Mzk5MDQuOTA4ODk2LCJzdWIiOiIyZTE5ZWY1Yy1iYTMwLTQ4NTEtYmQ2My01NWNhMzVjMDhmMzAiLCJzY29wZXMiOlsic2hpcHBpbmctY2FsY3VsYXRlIl19.Li3dIVZudbuahV8oyHmsrgPZ4d_EUsgcY7_0AhzRlycKCphVL15_bvc6k-sOef20uFUhXkGQHCMCckwcEM9VRXehS0htQqF0PaYLIIgq64nODs7f-upegGVHJNO1-xEnxQwFae7MqtbNfSGMTv09ejnw2BR4_hJwkbCjuhP7UfSJx2yV0mIZYlqoGrmrqU7GzdlmUdV1rqH-PC9AHCvUy_VICpd1eGylioo1KOofXP24yTMuZf8QYOUDFA_DUCWDrmJxfyOULe39JmSyuyi53G8fwqyVMBznS5X15pQL7WIFadl789XCbCeZnt7kaiz9Thy1YQY6lalae7bwCASmHmqb2uLCA_crn-VttLemlDSO1CuuBcM89x5lNHSQunxJxq34Xj4eWtZoI3sYb5xuAEJfrt6BIUlBizbb1Q6wwNQN0abGy8df8yHFMcqIkte7eCxvq5l9c0C-i6hHuglEBUYn6k9JTUeptei4s6ddelwpIat-Pat5glaQF4HP7iXrXjUrO_m3YCw3l-_FkdGhJ1wBzuCTEuRbqOw-DxCyJY-bbkpIeHGrBfjpQ0ZfUGT2F8ogmPlP7m8rReq07Pb34HGqNuzozvoE9PHAfSlrmuZd4wpxK54R17Yvekh69iOnHKpH9GxxH9l9YOEIIm0hfa8P6df87yTHy9ahQJZzmC4";
+
+
+    $peso = $_POST['peso'];
+    $largura = $_POST['largura'];
+    $altura = $_POST['altura'];
+    $comprimento = $_POST['comprimento'];
+    $valor = $_POST['valor'];
    
 
     $url = 'https://www.melhorenvio.com.br/api/v2/me/shipment/calculate';
@@ -42,25 +51,10 @@ function calcularFrete($peso, $largura, $altura, $comprimento, $valor) {
     // Faz a requisição
     $result = file_get_contents($url, false, $context);
 
+    // Retorna o resultado ao frontend do Shopify
     if ($result === FALSE) {
-        return ['error' => 'Erro ao calcular frete'];
+        echo json_encode(['error' => 'Erro ao calcular frete']);
+    } else {
+        echo $result;
     }
-
-    // Converte a resposta para um array
-    return json_decode($result, true);
-}
-
-// Exemplo de uso da função 
-$peso = 1;               // Peso em kg
-$largura = 11;             // Largura em cm
-$altura = 17;               // Altura em cm
-$comprimento = 11;         // Comprimento em cm
-$valor = 10.1; 
-
-// Executar o cálculo
-$resultadoFrete = calcularFrete($peso, $largura, $altura, $comprimento, $valor);
-
-// Enviar a resposta de volta para o Shopify
-header('Content-Type: application/json');
-echo json_encode($resultadoFrete);
 ?>
